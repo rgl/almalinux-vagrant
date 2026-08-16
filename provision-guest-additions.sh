ea8f75a2efe022fc4fcd6e49f1a53f51dbeb265b
Rui Lopes <rgl@ruilopes.com>
2026-08-16 22:31:16 +0000

    drop virtualbox leftovers

--- provision-guest-additions.sh
+++ provision-guest-additions.sh
@@ -2,25 +2,13 @@
 set -eux
 
 # install the Guest Additions.
-if [ -n "$(grep VBOX /sys/firmware/acpi/tables/APIC)" ]; then
-# install the VirtualBox Guest Additions.
-# this will be installed at /opt/VBoxGuestAdditions-VERSION.
-# REMOVE_INSTALLATION_DIR=0 is to fix a bug in VBoxLinuxAdditions.run.
-# See http://stackoverflow.com/a/25943638.
-dnf install -y kernel-headers kernel-devel gcc bzip2 tar make
-rpm -qa 'kernel*' | sort
-mkdir -p /mnt
-mount /dev/sr1 /mnt
-while [ ! -f /mnt/VBoxLinuxAdditions.run ]; do sleep 1; done
-# NB we ignore exit code 2 (system must be rebooted).
-REMOVE_INSTALLATION_DIR=0 /mnt/VBoxLinuxAdditions.run --target /tmp/VBoxGuestAdditions || [ $? -eq 2 ]
-rm -rf /tmp/VBoxGuestAdditions
-umount /mnt
-eject /dev/sr1
-modinfo vboxguest
-else
+if [ "$(systemd-detect-virt)" == "kvm" ]; then
 # install the qemu-kvm Guest Additions.
+# see https://packages.fedoraproject.org/pkgs/qemu/qemu-guest-agent/
+# see https://packages.fedoraproject.org/pkgs/spice-vdagent/spice-vdagent/
 dnf install -y qemu-guest-agent spice-vdagent
+else
+echo 'ERROR: Unknown VM host.' || exit 1
 fi
 
 # reboot.
