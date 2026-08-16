688bbf8aa144af51a4ca42a5d1e43742d7e31507
Rui Lopes <rgl@ruilopes.com>
2026-08-16 22:31:16 +0000

    add support for cloud-init

--- provision.sh
+++ provision.sh
@@ -30,6 +30,9 @@ chmod 600 authorized_keys
 chown -R vagrant:vagrant .
 popd
 
+# install cloud-init.
+dnf -y install cloud-init
+
 # install the nfs client to support nfs synced folders in vagrant.
 dnf -y install nfs-utils
 
